ec7ea5026bb8345cfe5ddf25da254b7375b2a77f
Betagramcito <jbetancur976@soyudemedellin.edu.co>
2024-09-16 19:54:08 -0500

    Rotación
    Se le  agregó la rotación al personaje

--- BoomTank/Assets/Scripts/Movimiento.cs
+++ BoomTank/Assets/Scripts/Movimiento.cs
@@ -5,22 +5,45 @@ using UnityEngine;
 public class Movimiento : MonoBehaviour
 {
     [SerializeField] private float velocidadMovimiento;
+    [SerializeField] private float velocidadRotacion = 90f; 
     [SerializeField] private Vector2 direccion;
     private Rigidbody2D rb2D;
+    private float anguloRotacion = 0f; // Ángulo de rotación inicial (mirando al norte)
 
     private void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
+        transform.rotation = Quaternion.Euler(0, 0, 0); // Asegura que el tanque inicie mirando al norte
     }
 
     private void Update()
     {
         direccion = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+
+        // Cambia el ángulo de rotación según la tecla presionada
+        if (Input.GetKey(KeyCode.W))
+        {
+            anguloRotacion = 0f; // Norte
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            anguloRotacion = -90f; // Este (rotación en sentido horario)
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            anguloRotacion = -180f; // Sur
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            anguloRotacion = -270f; // Oeste
+        }
+
+        // Aplica la rotación al objeto
+        transform.rotation = Quaternion.Euler(0, 0, anguloRotacion);
     }
 
     private void FixedUpdate()
     {
-        rb2D.MovePosition(rb2D.position+direccion*velocidadMovimiento*Time.fixedDeltaTime);
+        rb2D.MovePosition(rb2D.position + direccion * velocidadMovimiento * Time.fixedDeltaTime);
     }
 }
-
